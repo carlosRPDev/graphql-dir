07eade806213c983b73fc59aa5eadd7317e4ef1a
Carlos Rodriguez <ucroxlive@gmail.com>
2021-12-03 11:15:13 -0500

    Aprendimos a configurar un proyecto, a conocer los tipos y realizar una query

--- index.js
+++ index.js
@@ -1,50 +1,37 @@
-"use strict";
+'use strict'
 
 // Estamos requiriendo las librerias que se va a usar desde la dependencia de GraphQl
-const { graphql, buildSchema } = require("graphql");
+const { buildSchema } = require('graphql')
 // requerir las librerias que necesitamos para ejecutar express
-const express = require("express");
-const gqlMiddleware = require("express-graphql");
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+// librerias para leer desde otra ubicacion el archivo schema
+const { readFileSync } = require('fs')
+const { join } = require('path')
+// importamos los resilvers
+const resolvers = require('./lib/resolvers')
 
 // Luego de requerir express debemos crear una API de express
-const app = express();
+const app = express()
 // se configura el puerto de escucha de api
-const port = process.env.port || 3000;
+const port = process.env.port || 3000
 
 // Definiendo el esquema
-const schema = buildSchema(`
-  type Query {
-    "Retorna un saludo al mundo"
-    hello: String
-  }
-`);
-
-// Configurar los resolver
-const resolvers = {
-  hello: () => {
-    return "Hola Mundo";
-  },
-  //saludo: () => {
-  //return "Hola a todos!";
-  //},
-};
-
-// Ejecutndo el query hello
-//graphql(schema, "{ hello }", resolver).then((data) => {
-//  console.log(data);
-//});
+const schema = buildSchema(
+  readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+)
 
 // Configuramos el midleware para la ejecucion de los resolvers
 app.use(
-  "/api",
+  '/api',
   gqlMiddleware({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true,
+    graphiql: true
   })
-);
+)
 
 // Por ultimo es cionfigurar lo de la escucha
 app.listen(port, () => {
-  console.log(`server is listening at http://localhost:${port}/api`);
-});
+  console.log(`server is listening at http://localhost:${port}/api`)
+})
