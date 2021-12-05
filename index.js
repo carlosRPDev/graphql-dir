a9890f3f8465a8328b4007663d211c116289ae04
Carlos Rodriguez <ucroxlive@gmail.com>
2021-12-04 19:59:34 -0500

    Crear un query que recibe parametros para consultar informacion especifica

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // Estamos requiriendo las librerias que se va a usar desde la dependencia de GraphQl
-const { buildSchema } = require('graphql')
+const { makeExecutableSchema } = require('graphql-tools')
 // requerir las librerias que necesitamos para ejecutar express
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
@@ -17,9 +17,11 @@ const app = express()
 const port = process.env.port || 3000
 
 // Definiendo el esquema
-const schema = buildSchema(
-  readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
 )
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 // Configuramos el midleware para la ejecucion de los resolvers
 app.use(
